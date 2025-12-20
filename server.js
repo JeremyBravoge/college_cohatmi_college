5800397fe705c2172ddb231f09a513d71d43f1ec
Jeremy Bravoge <bravogejeremy@gmail.com>
2025-12-20 05:45:47 +0300

    live update

--- server.js
+++ server.js
@@ -62,5 +62,7 @@ app.use('/api/reports', reportRouter);
 app.use('/api/ranking', rankingRoutes);
 
 
-const PORT = 5000;
+// Use Render's dynamic port or fallback to 5000 for local development
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
